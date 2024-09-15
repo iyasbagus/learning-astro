40db9f5d35e5b9d4e47ef2d2ce4904c7dd0d067a
iyas bagus <bagusiyas15@gmail.com>
2024-09-15 20:49:44 +0700

    belajar astro dengan template nya

---
+++ latihan-template/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Iyas Docs',
+			social: {
+				github: 'https://github.com/iyasbagus',
+			},
+			sidebar: [
+				{
+					label: 'Start Here',
+					autogenerate: { directory: 'start-here' },
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
---
+++ latihan-template/src/content/docs/start-here/manual.md
@@ -0,0 +1,14 @@
+---
+title: Manual Setup
+description: A reference page in my new Starlight docs site.
+---
+
+The quickest way to create a new Starlight site is using create <code dir="auto">astro</code> as shown in the <a href="">Getting Started guide.</a> If you want to add Starlight to an existing Astro project, this guide will explain how.
+
+## Set Up Starlight
+
+To follow this guide, you’ll need an existing Astro project.
+
+# Add the Starlight integration
+
+Starlight is an <a href="">Astro integration.</a> Add it to your site by running the <code dir="auto">astro add</code> command in your project’s root directory:
